b0371d9631ca1241d880fb8f0d798b0d19004b8b
Stephen Winsor <stephen.g.winsor@gmail.com>
2016-07-12 14:46:58 -0400

    Updated to match new menus markup.

--- js/stories.js
+++ js/stories.js
@@ -5,19 +5,17 @@
     var currentPathName = location.pathname;
     $('a[href="'+currentPathName+'"]').addClass('active');
 
-    $(window).scroll(function(){
-      console.log('ok')
-      if(window.scrollY > 120) {
-        $('#mobile-nav').addClass('at-top')
-      } else {
-        $('#mobile-nav').removeClass('at-top')
-      }
-    })
+    // $(window).scroll(function(){
+    //   if(window.scrollY > 120) {
+    //     $('#mobile-nav').addClass('at-top')
+    //   } else {
+    //     $('#mobile-nav').removeClass('at-top')
+    //   }
+    // })
 
     // Open the mobile menu
     $('.menu-button').click(function(e){
       e.preventDefault();
-      $('#mobile-fullscreen-menu').addClass('visible')
       $('body').toggleClass('mobile-nav-visible')
     })
 
@@ -25,7 +23,6 @@
     // Close the mobile menu
     $('.close-button').click(function(e){
       e.preventDefault();
-      $('#mobile-fullscreen-menu').removeClass('visible');
       $('body').removeClass('mobile-nav-visible')
     })
 
@@ -33,15 +30,13 @@
     // Close the menu when someone clicks on the
     // contact link (if the menu is open)
     $('a[href="#contact"]').click(function(e){
-      $('#mobile-fullscreen-menu').removeClass('visible')
       $('body').removeClass('mobile-nav-visible')
     })
 
     // Hide menu if the user resizes the menu into desktop
     // layout while the mobile fullscreen menu is open
     $(window).resize(function(e){
-      if( $('#mobile-fullscreen-menu').hasClass('visible') ) {
-        $('#mobile-fullscreen-menu').removeClass('visible');
+      if( $('body').hasClass('mobile-nav-visible') ) {
         $('body').removeClass('mobile-nav-visible')
       }
     })
